7a4be18096d232041de0d9e15aac31fa216841b6
Tet <Maria-Theresa.Edralin@db.com>
2022-11-30 15:39:19 -0500

    folder structure

---
+++ src/main/java/com/nology/synergyapi/controller/CurrencyController.java
@@ -0,0 +1,12 @@
+package com.nology.synergyapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins  =  "http://localhost:3000")
+public class CurrencyController {
+//    @GetMapping("/currencies")
+}
---
+++ src/main/java/com/nology/synergyapi/model/Currency.java
@@ -0,0 +1,12 @@
+package com.nology.synergyapi.model;
+
+public class Currency {
+//    @Id
+    private String code;
+
+//           "Flag": "https://www.currencyremitapp.com/wp-content/themes/currencyremitapp/images/countryimages/albania.png",
+//               "CountryName": "Albania",
+//               "Currency": "Lek",
+//               "Code": "ALL",
+//               "Symbol": "Lek"
+}
