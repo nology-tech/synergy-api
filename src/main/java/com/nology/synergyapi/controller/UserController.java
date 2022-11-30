89d8c1e9b0cd9134a0e03bd078ccb63f4d87b23c
ppatilgit <pavitra.raleigh@gmail.com>
2022-11-30 17:16:23 -0500

    created UserProfile

--- src/main/java/com/nology/synergyapi/controller/UserController.java
+++ src/main/java/com/nology/synergyapi/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.nology.synergyapi.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+
 }
---
+++ src/main/java/com/nology/synergyapi/model/UserProfile.java
@@ -0,0 +1,57 @@
+package com.nology.synergyapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "user_id", nullable = false)
+    private Long user_id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String pwd;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+}
