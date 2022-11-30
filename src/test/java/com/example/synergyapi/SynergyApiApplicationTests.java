bcd5d02bc89b12a2bb1d355dc554132fba3a5d7b
Sheena Abraham <SheenaPAbraham@gmail.com>
2022-11-30 14:11:40 -0500

    initial commit

---
+++ src/test/java/com/example/synergyapi/SynergyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.synergyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
