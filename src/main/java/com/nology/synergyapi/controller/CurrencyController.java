bdaf7ee03420e76471081e617b819f7539e88006
Renata Yakobov <rinat.yuri@gmail.com>
2022-12-01 14:48:30 -0500

    added get one currency API end point

--- src/main/java/com/nology/synergyapi/controller/CurrencyController.java
+++ src/main/java/com/nology/synergyapi/controller/CurrencyController.java
@@ -1,23 +1,35 @@
 package com.nology.synergyapi.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import com.nology.synergyapi.model.Currency;
 import com.nology.synergyapi.service.CurrencyService;
-import org.h2.util.json.JSONObject;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins  =  "http://localhost:3000")
+@CrossOrigin(origins  =  "http://localhost:8080")
 public class CurrencyController {
     @GetMapping("/currencies")
     public List<Currency> getAllCurrencies() throws IOException {
         return CurrencyService.getCurrencyData();
     }
+
+    @GetMapping("/currencies/{myCode}")
+    public Currency getCurrencyByCode(@PathVariable String myCode) throws IOException {
+        return CurrencyService.getCurrencyData().stream().filter(currency -> currency.getCode().equals(myCode)).collect(Collectors.toList()).get(0);
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/nology/synergyapi/service/CurrencyService.java
+++ src/main/java/com/nology/synergyapi/service/CurrencyService.java
@@ -3,13 +3,7 @@ package com.nology.synergyapi.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nology.synergyapi.model.Currency;
-import org.apache.tomcat.util.json.JSONParser;
-import org.h2.util.json.JSONArray;
-import org.h2.util.json.JSONObject;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
