83b16782bf0cc54f25b02776ed1efcea99355132
Tina Samuels <mssamuelstina@gmail.com>
2022-11-30 23:11:34 -0500

    added Bank model

--- src/main/java/com/nology/synergyapi/controller/BankController.java
+++ src/main/java/com/nology/synergyapi/controller/BankController.java
@@ -5,23 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
 public class BankController {
+    private ArrayList<Bank> banks = new ArrayList<>();
 
-    @GetMapping("/Bank")
-    public String bankDetails() {
-        return "Below are the Bank Details!";
+    //CREATE route
+    @PostMapping("/banks")
+    public String createBank(@RequestBody Bank bank) {
+        banks.add(bank);
+        return "Bank added";
     }
 
-    @GetMapping("/Bank/{name}")
-    public ResponseEntity<Optional<Bank>> getBankByName(@PathVariable String name) {
+//    @GetMapping("/Bank")
+//    public String bankDetails() {
+//        return "Below are the Bank Details!";
+//    }
+//
+//    @GetMapping("/Bank/{name}")
+//    public ResponseEntity<Optional<Bank>> getBankByName(@PathVariable String name) {
 //        return greetings.stream()
 //                .filter(greeting -> greeting.getId() == parseInt(id))
 //                .findFirst()
@@ -30,6 +37,6 @@ public class BankController {
         //ResponseEntity
         // configure the entire response using this
         // .status() configure the status code we receive
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findByName(name));
-    }
+        //return ResponseEntity.status(HttpStatus.OK).body(repository.findByName(name));
+    //}
 }
--- src/main/java/com/nology/synergyapi/model/Bank.java
+++ src/main/java/com/nology/synergyapi/model/Bank.java
@@ -1,4 +1,60 @@
 package com.nology.synergyapi.model;
 
+import java.sql.Timestamp;
+
 public class Bank {
+    private  String bankName;
+    private String createdBy;
+    private String logo;
+    private Timestamp dateCreated;
+
+    public Bank(String bankName, String createdBy, String logo, Timestamp dateCreated) {
+        this.bankName = bankName;
+        this.createdBy = createdBy;
+        this.logo = logo;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "bankName='" + bankName + '\'' +
+                ", createdBy='" + createdBy + '\'' +
+                ", logo='" + logo + '\'' +
+                ", dateCreated=" + dateCreated +
+                '}';
+    }
 }
