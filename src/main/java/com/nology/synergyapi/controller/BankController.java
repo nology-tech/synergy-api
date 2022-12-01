23162f6e97a7f5347cddfea68e417fe147d7c61c
Tina Samuels <mssamuelstina@gmail.com>
2022-12-01 16:14:27 -0500

    created BankService and deleted BankRepository

--- src/main/java/com/nology/synergyapi/controller/BankController.java
+++ src/main/java/com/nology/synergyapi/controller/BankController.java
@@ -1,46 +1,54 @@
 package com.nology.synergyapi.controller;
 
-import com.nology.synergyapi.BankRepository;
 import com.nology.synergyapi.model.Bank;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nology.synergyapi.service.BankService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.Optional;
 
 
+
+//    private List<Bank> banks = new ArrayList<>();
 @RestController
-public class BankController {
-    private List<Bank> banks = new ArrayList<>();
+    @CrossOrigin(origins  =  "http://localhost:8080")
+    public class BankController {
+        @GetMapping("/banks")
+        public List<Bank> getAllCurrencies() throws IOException {
+            return BankService.getBankData();
+        }
 
-
-//    @Autowired
-     BankRepository repository;
+//@Autowired
+     //BankRepository repository;
 
 
     //CREATE route
-    @PostMapping("/banks")
-    public Bank createBank(@RequestBody Bank bank) {
-        banks.add(bank);
-        return bank;
-    }
+//    @PostMapping("/banks")
+//    public Bank createBank(@RequestBody Bank bank) {
+//       //Bank.save(bank);
+//        return bank;
+//    }
 
-    @GetMapping("/banks")
-    public String allBankDetails() {
-    return "This is a list of Bank details";
-  //       return repository.findAll();
-        }
+//    @GetMapping("/banks")
+//    public <Bank> String allBankDetails() {
+//   // return "This is a list of Bank details";
+//        return ;
+//        }
+
+
+
 
 //    @GetMapping("/Bank/{name}")
 //    public ResponseEntity<Optional<Bank>> getBankByName(@PathVariable String name) {
-//        return greetings.stream()
-//                .filter(greeting -> greeting.getId() == parseInt(id))
-//                .findFirst()
-//                .orElse(null);
-
-        //ResponseEntity
-        // configure the entire response using this
-        // .status() configure the status code we receive
-        //return ResponseEntity.status(HttpStatus.OK).body(repository.findByName(name));
-    //}
+//
+//        //ResponseEntity
+//        // configure the entire response using this
+//        // .status() configure the status code we receive
+//        return ResponseEntity.status(HttpStatus.OK).body(repository.findByName(name));
+//    }
 }
