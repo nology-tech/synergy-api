64c76a921910a6c8abb01f6570c978c8ab9e1916
patwork365 <patwork365@gmail.com>
2022-12-01 17:51:14 -0500

    Get user by ID

--- src/main/java/com/nology/synergyapi/controller/UserController.java
+++ src/main/java/com/nology/synergyapi/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.nology.synergyapi.controller;
 
-import com.nology.synergyapi.model.Currency;
 import com.nology.synergyapi.model.UserProfile;
-import com.nology.synergyapi.service.CurrencyService;
 import com.nology.synergyapi.service.UserService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins  =  "http://localhost:8080")
@@ -19,4 +19,8 @@ public class UserController {
         return UserService.getUserData();
     }
 
+    @GetMapping("/users/{uid}")
+    public Optional<UserProfile> getUserByID (@PathVariable Long uid) throws IOException {
+        return UserService.getUserByID(uid);
+    }
 }
