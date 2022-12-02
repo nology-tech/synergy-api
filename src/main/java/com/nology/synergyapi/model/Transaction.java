589a0dfb8a7bd6cd571e80a8cbd0c041000d0ac9
shirinnizan <shirinnizan@gmail.com>
2022-12-02 16:19:25 -0500

    transaction data added

--- src/main/java/com/nology/synergyapi/model/Transaction.java
+++ src/main/java/com/nology/synergyapi/model/Transaction.java
@@ -1,4 +1,10 @@
 package com.nology.synergyapi.model;
 
 public class Transaction {
+
+    // to
+    // from
+    // amount
+    // currency
+    // boolean withdraw and deposit  (if the request from me - withdraw, to me - deposit)
 }
