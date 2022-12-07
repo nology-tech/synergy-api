3394c4cfaea3300a9335284935d80959e164fcb3
Renata Yakobov <rinat.yuri@gmail.com>
2022-12-06 19:57:40 -0500

    moved enriching currency name to currency controller

--- src/main/java/com/nology/synergyapi/controller/CurrencyRateController.java
+++ src/main/java/com/nology/synergyapi/controller/CurrencyRateController.java
@@ -19,26 +19,18 @@ import java.util.stream.Collectors;
 public class CurrencyRateController {
     @GetMapping("/currencyrates")
     public List<CurrencyRate> getAllCurrencyRates() throws IOException {
-        List<Currency> currencydetails = CurrencyService.getCurrencyData();
-        String[][] currencynames = CurrencyNamesService.getCurrencyNames().getSupported_codes();
-        // convert array of arrays of names to a hashmap so we can find a currency code / currency name pair
-        Map<String, String> hashMapNames = new HashMap<String, String>();
-        for (String[] name :currencynames) {
-            String ccode = name[0];
-            String cname = name[1];
-            hashMapNames.put(ccode, cname);
-        }
+        List<Currency> currencydetails = CurrencyController.getAllCurrencies();
         HashMap<String, Double> rates = FXRateService.getFXRateData().getConversion_rates();
         // adding USD manually as a first element as it cannot cast rate 1 to double
         Currency usdCurrency = currencydetails.stream().filter(currency -> currency.getCode().equals("USD")).collect(Collectors.toList()).get(0);
-        CurrencyRate usdCurrencyRate = new CurrencyRate(usdCurrency.getCode(), usdCurrency.getFlag(), usdCurrency.getCountryName(), hashMapNames.get(usdCurrency.getCode()), usdCurrency.getSymbol(), 1.000);
+        CurrencyRate usdCurrencyRate = new CurrencyRate(usdCurrency.getCode(), usdCurrency.getFlag(), usdCurrency.getCountryName(), usdCurrency.getCurrency(), usdCurrency.getSymbol(), 1.000);
         List<CurrencyRate> currencyRates = new ArrayList<>();
         currencyRates.add(usdCurrencyRate);
 
         // construct rates array with currency details
         currencydetails.forEach (currency -> {
              if (rates.get(currency.getCode())!=null && !currency.getCode().equals("USD")) {
-                 CurrencyRate currencyRate = new CurrencyRate(currency.getCode(), currency.getFlag(), currency.getCountryName(), hashMapNames.get(currency.getCode()), currency.getSymbol(), rates.get(currency.getCode()));
+                 CurrencyRate currencyRate = new CurrencyRate(currency.getCode(), currency.getFlag(), currency.getCountryName(), currency.getCurrency(), currency.getSymbol(), rates.get(currency.getCode()));
                  currencyRates.add(currencyRate);
              }
         });
