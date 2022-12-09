53d2006203de2e51660b0fe39a4bf4295ad9c793
Tet <Maria-Theresa.Edralin@db.com>
2022-12-09 07:56:39 -0500

    changed cross origin in BankController.java

--- src/main/java/com/nology/synergyapi/controller/BankController.java
+++ src/main/java/com/nology/synergyapi/controller/BankController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 
 @RestController
-    @CrossOrigin(origins  =  "http://localhost:8080")
+    @CrossOrigin(origins  =  "http://localhost:3000")
     public class BankController {
     @GetMapping("/banks")
     public List<Bank> getAllBanks() throws IOException {
