3bc1d4c83d24ff6684f81ca049e8f239233585b3
Eva S <evamarie.songco@gmail.com>
2022-12-14 07:34:26 -0800

    parameterize frontend.url

--- src/main/java/com/nology/synergyapi/controller/UserController.java
+++ src/main/java/com/nology/synergyapi/controller/UserController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins  = {"http://localhost:3000"})
+@CrossOrigin(origins  = "${frontend.url}")
 //@Entity
 //@Table(name = "ARTICLES")
 public class UserController {
